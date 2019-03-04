8d6e133f0983795bb6c10d0f7672db71f6a99d1a
mellamocasamundo <casey_henderson@davincischools.org>
2019-03-03 18:16:06 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 a = document.getElementById("ice1");
 b = document.getElementById("ice2");
 c = document.getElementById("ice3");
-var mySound = document.getElementById('you-win');
+var mySound = document.getElementById('cracking');
 
 function nextImage(el){
 	if (el.src.match("image/ice.png")){ 
 		el.src = "image/ice (1).jpg";
 	} else if (el.src.match("image/ice(1).jpg")){ 
 		el.src = "image/dirt.jpg";
+	} else if {el.src.math("image.dirt.jpg")){
+		el.src = "image/ice.png";
 	} else {
 	}
 }
 
 function checkOrder(el){
-	if (a.src.match("image/dirt.jpg") && b.src.match("image/ice.png") && c.src.match("image/ice.png")){
-		alert("JACKPOT");
-		a.classList.add("opaque");
-		b.classList.add("opaque");
-		c.classList.add("opaque");
+	if (a.src.match("image/dirt.jpg") && b.src.match("image/dirt.jpg") && c.src.match("image/dirt.jpg")){
+		alert("Scrat has his acorn!")
 	} else {
-		alert("BETTER LUCK NEXT TIME");
 	}
 }
