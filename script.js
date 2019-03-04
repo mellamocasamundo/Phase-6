b44bd441afb814de13121a3a5d998a9cb77ee2d0
mellamocasamundo <casey_henderson@davincischools.org>
2019-03-04 14:08:38 -0800

    Update script.js

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ function nextImage(el){
 
 function checkOrder(el){
 	if (a.src.match("image/dirt.jpg") && b.src.match("image/dirt.jpg") && c.src.match("image/dirt.jpg")){
-		el.src = "home (1).png";
-		d.src = "sky (1).png"
+		el.src = "home1.png";
+		d.src = "sky1.png"
 		mySound1.play();
 		alert("Scrat has his acorn!");
 	} else {
