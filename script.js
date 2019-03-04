5ce429786842011a6dc86931457d9e727b0e041d
mellamocasamundo <casey_henderson@davincischools.org>
2019-03-03 18:18:35 -0800

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,13 @@ var mySound = document.getElementById('cracking');
 function nextImage(el){
 	if (el.src.match("image/ice.png")){ 
 		el.src = "image/ice (1).jpg";
+		mySound.play();
 	} else if (el.src.match("image/ice(1).jpg")){ 
 		el.src = "image/dirt.jpg";
+		mySound.play();
 	} else if (el.src.match("image.dirt.jpg")){
 		el.src = "image/ice.png";
+		mySound.play();
 	} else {
 	}
 }
