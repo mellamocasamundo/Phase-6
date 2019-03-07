52b2d37487bfe637a6cceec85370c63dd554c38d
mellamocasamundo <casey_henderson@davincischools.org>
2019-03-07 14:32:57 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,33 @@ a = document.getElementById("ice1");
 b = document.getElementById("ice2");
 c = document.getElementById("ice3");
 d = document.getElementById("sky");
+e = document.getElementById("key");
 var mySound = document.getElementById('cracking');
 var mySound1 = document.getElementById('scrat');
 
 function nextFirstImage(el){
 	if (el.src.match("image/ice.png")){ 
+		el.src = "image/ice1.png";
+		mySound.play();
+	} else if (el.src.match("image/ice1.png")){ 
+		el.src = "image/dirt.png";
+		mySound.play();
+	} else {
+	}
+}
+
+function nextSecondImage(el){
+	if (c.src.match("image/dirt.png")){
+		el.src = "image/ice1.png";
+		mySound.play();
+	} else if (el.src.match("image/ice1.png")){ 
+		el.src = "image/dirt.png";
+		mySound.play();
+	} else {
+	}
+}
+function nextThirdImage(el){
+	if (b.src.match("image/dirt.png") && c.src.match("image/dirt.png")){
 		el.src = "image/ice1.png";
 		mySound.play();
 	} else if (el.src.match("image/ice1.png")){ 
@@ -22,31 +44,7 @@ function checkOrder(el){
 		d.src = "image/sky1.png"
 		mySound1.play();
 		alert("Scrat has his acorn!");
-	} else {
-		mySound1.play();
-		alert("Scrat wants his acorn!");
-	}
-}
-function checkOrder(el){
-	if (a.src.match("image/dirt.png") && b.src.match("image/dirt.png") && c.src.match("image/dirt.png")){
-		el.src = "image/home1.png";
-		d.src = "image/sky1.png"
-		mySound1.play();
-		alert("Scrat has his acorn!");
-	} else {
-		mySound1.play();
-		alert("Scrat wants his acorn!");
-	}
-}
-
-
-function checkOrder(el){
-	if (a.src.match("image/dirt.png") && b.src.match("image/dirt.png") && c.src.match("image/dirt.png")){
-		el.src = "image/home1.png";
-		d.src = "image/sky1.png"
-		mySound1.play();
-		alert("Scrat has his acorn!");
-		document.getElementById("sky2").src="image/boss_key.gif"
+		
 	} else {
 		mySound1.play();
 		alert("Scrat wants his acorn!");
